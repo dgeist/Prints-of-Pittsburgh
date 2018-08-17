ebbd33cc9bf47c414d6acb0390f6eab973fa1e08
Jake Smith <jsmith21317@gmail.com>
2018-08-16 23:04:45 -0400

    fixed/mobile nav
    -still needs layout work

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-﻿
+﻿$(document).ready(function () {
+    $(window).resize(function () {
+        responsiveNav();
+    });
+    responsiveNav();
+});
+
+function toggleNav() {
+    $('.nav-links').slideToggle(400);
+    $('.nav-icon').toggleClass('open');
+}
+
+function responsiveNav() {
+    console.log(window.innerWidth);
+    if (window.innerWidth <= 1000) {
+        $('.nav-links').css('display', 'none');
+        $('.topnav').addClass('mobile');
+    } else {
+        $('.nav-links').css('display', 'block');
+        $('.topnav').removeClass('mobile');
+        $('.nav-icon').removeClass('open');
+    }
+}
